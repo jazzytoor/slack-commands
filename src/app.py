3c1c6740b3536f249903301e4e3e011eabf7ef9b
jazz toor <jazztoor@jazz.local>
2023-11-04 21:32:54 +0000

    feat: add another command

--- src/app.py
+++ src/app.py
@@ -1,30 +1,35 @@
 from flask import Flask, request
 from slack_bolt import App
 from slack_bolt.adapter.flask import SlackRequestHandler
+import requests
+import logging
 
 flask_app = Flask(__name__)
 bolt_app = App()
 handler = SlackRequestHandler(bolt_app)
 
-
-@bolt_app.middleware
-def log_request(logger, body, next):
-    logger.debug(body)
-    return next()
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
 
 @bolt_app.event("app_mention")
 def event_test(body, say, logger):
-    logger.info(body)
-    say("")
+    logger.debug(body)
+
+    say("Hey! How can I help? :thinking_face:")
 
 
-# @bolt_app.command("/<>")
-# def command(ack, body, respond):
-#     ack()
-#     respond("<>")
+@bolt_app.command("/motivate")
+def motivate_command(ack, respond):
+    ack()
+
+    quote = requests.get("https://zenquotes.io/api/today")
+    if quote.status_code == 200:
+        respond(f"*{quote.json()[0]['q']}*")
+    else:
+        respond("No quote! :cry:")
 
 
-@flask_app.route("/commands", methods=["POST"])
+@flask_app.route("/slack/events", methods=["POST"])
 def slack_events():
     return handler.handle(request)
